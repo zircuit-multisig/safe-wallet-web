f300325126419759bee992debe5138717c221777
Leoni Mella <leoni.mella@gmail.com>
2024-07-17 12:04:03 -0300

    feat: update gateway and chainId for production

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,9 +4,9 @@ export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 
 export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.testnet.safe.zircuit.com'
+  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.safe.zircuit.com'
 export const GATEWAY_URL_STAGING =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.testnet.safe.zircuit.com'
+  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.staging.safe.zircuit.com'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
